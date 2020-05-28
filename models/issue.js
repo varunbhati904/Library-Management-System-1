7ed649ce01756005676c3f18c1e2d678c34af23f
varunbhati904 <bhativarun1234@gmail.com>
2020-05-28 11:20:07 +0530

    add some new

--- models/issue.js
+++ models/issue.js
@@ -2,5 +2,7 @@ var mongoose = require("mongoose");
 var IssueSchema = new mongoose.Schema({
 AccNo : {type:String},
 username : {type:String},
+issud_on:{type:Date,default:Date.now},
+Due_on : {type:Date,default:Date.now()+ 30*24*60*60*1000}
 });
 module.exports = mongoose.model("Issue", IssueSchema);
