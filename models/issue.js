be2e1628b6cdc6b6f3f7c07a6fb774950e71a4c9
varunbhati904 <bhativarun1234@gmail.com>
2020-05-28 11:49:13 +0530

    add some new

--- models/issue.js
+++ models/issue.js
@@ -1,9 +1,10 @@
 var date = new Date();
+var ndate = new Date();
 var mongoose = require("mongoose");
 var IssueSchema = new mongoose.Schema({
 AccNo : {type:String},
 username : {type:String},
 issud_on:{type:Date,default:date},
-Due_on : {type:Date,default:date.setDate(date.getDate()+ 30)}
+Due_on : {type:Date,default:ndate.setDate(dnate.getDate()+ 30)}
 });
 module.exports = mongoose.model("Issue", IssueSchema);
