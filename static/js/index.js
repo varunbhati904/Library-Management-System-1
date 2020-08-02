6c1796e25b3d874ded197fa9b055b3096faa902e
ChandanSitlani <40667874+ChandanSitlani@users.noreply.github.com>
2020-08-02 13:26:39 +0530

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,79 @@
+
+try {
+  var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+  var aurecognition = new SpeechRecognition();
+}
+catch(e) {
+  console.error(e);
+  $('.no-browser-support').show();
+  $('.app').hide();
+}
+
+
+var aunoteTextarea = $('#aunote-textarea');
+var instructions = $('#recording-instructions');
+var notesList = $('ul#notes');
+
+var aunoteContent = '';
+
+// Get all notes from previous sessions and display them.
+
+
+recognition.onresult = function(event) {
+	consol.log(event);
+
+  var current = event.resultIndex;
+
+  // Get a transcript of what was said.
+  var transcript = event.results[current][0].transcript;
+
+  // Add the current transcript to the contents of our Note.
+  // There is a weird bug on mobile, where everything is repeated twice.
+  // There is no official solution so far so we have to handle an edge case.
+  var mobileRepeatBug = (current == 1 && transcript == event.results[0][0].transcript);
+
+  if(!mobileRepeatBug) {
+    noteContent += transcript;
+    noteTextarea.val(noteContent);
+  }
+};
+
+recognition.onstart = function() { 
+  instructions.text('Voice recognition activated. Try speaking into the microphone.');
+}
+
+recognition.onspeechend = function() {
+  instructions.text('You were quiet for a while so voice recognition turned itself off.');
+}
+
+recognition.onerror = function(event) {
+  if(event.error == 'no-speech') {
+    instructions.text('No speech was detected. Try again.');  
+  };
+}
+
+
+
+
+
+$('#austart-record-btn').on('click', function(e) {
+	console.log("ok");
+  if (noteContent.length) {
+    aunoteContent += ' ';
+  }
+  aurecognition.start();
+});
+
+$('#aupause-record-btn').on('click', function(e) {
+  aurecognition.stop();
+  instructions.text('Voice recognition paused.');
+});
+
+
+
+
+//-----------------------book--------------------------
+
 try {
   var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
   var recognition = new SpeechRecognition();
