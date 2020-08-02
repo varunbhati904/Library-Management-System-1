d23f6c147f3a01666d74cb16ea2942cf9e40e839
varunbhati904 <bhativarun1234@gmail.com>
2020-08-02 13:52:07 +0530

    logout

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ app.post("/login",function(req,res){
 
   });
 	app.get('/logout',function(req,res){
-	    req.logout(function(err){
+	    req.session.destroy(function(err){
 	        if(err){
 	            console.log(err);
 	        }
