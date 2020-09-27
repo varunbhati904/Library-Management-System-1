25baca4f8965de93859d288dc8f4a7099026bc15
unknown <sitlanic@gmail.com>
2020-09-27 18:11:02 +0530

    made responsive

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 var passportLocalMongoose = require("passport-local-mongoose");
 var UserSchema = new mongoose.Schema({
 name :{type:String},
-rollno :{type:String,unique:true},
+rollno :{type:String},
 DOB : {type:Date},
 Fine : {type:Number,default:0},
 email : {type:String},
